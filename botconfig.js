e75b1d47c02497d54beb5d17e5955c36f363704c
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-02-26 15:55:38 -0800

    Config

--- botconfig.js
+++ botconfig.js
@@ -2,13 +2,13 @@ module.exports = {
   Admins: ["173634281116008448"], //Admins of the bot
   ExpressServer: true, //If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || "!", //Default prefix, Server Admins can change the prefix
-  Port: 3033, //Which port website gonna be hosted
+  Port: 80, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/garrysmod", //Support Server Link
   Token: process.env.Token || "", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
-  ServerDeafen: true, //If you want bot to stay deafened
+  ServerDeafen: falase, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
   "24/7": false, //If you want the bot to be stay in the vc 24/7
